c3b05a1431edd6fbb6a67676c228f0c774ff6c8d
Soh Hong Yu <sohhongyu@gmail.com>
2024-09-06 18:33:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "@/pages/Layout";
 import Error from "@/pages/404";
 import Home from "@/pages/Home";
-import Player from "@/pages/P2ayer";
+import Player from "@/pages/Player";
 
 function App() {
   return (
